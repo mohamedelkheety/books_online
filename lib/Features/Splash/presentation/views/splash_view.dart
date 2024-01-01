25580b21e33849334febe911866e210492f818ff
mohamedelkheety <mohamedelkheaty97@gmail.com>
2024-01-01 14:43:40 +0200

    add animation to splash view

--- lib/Features/Splash/presentation/views/splash_view.dart
+++ lib/Features/Splash/presentation/views/splash_view.dart
@@ -7,7 +7,27 @@ class SplashView extends StatefulWidget {
   State<SplashView> createState() => _SplashViewState();
 }
 
-class _SplashViewState extends State<SplashView> {
+class _SplashViewState extends State<SplashView> with TickerProviderStateMixin {
+  late AnimationController animationController;
+  late Animation<Offset> animation;
+  @override
+  void initState() {
+    super.initState();
+    animationController = AnimationController(
+      duration: const Duration(seconds: 1),
+      vsync: this,
+    );
+    animation = Tween<Offset>(begin: const Offset(0, 3), end: Offset.zero)
+        .animate(animationController);
+    animationController.forward();
+  }
+
+  @override
+  void dispose() {
+    animationController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -19,14 +39,23 @@ class _SplashViewState extends State<SplashView> {
             'assets/images/logo.png',
             height: MediaQuery.of(context).size.height * 0.30,
           ),
-          const Text(
-            'Read Free Books',
-            textAlign: TextAlign.center,
-            style: TextStyle(
-              fontSize: 36,
-              fontFamily: 'Pacifico',
-            ),
-          )
+          Builder(builder: (context) {
+            return AnimatedBuilder(
+                animation: animation,
+                builder: (context, _) {
+                  return SlideTransition(
+                    position: animation,
+                    child: const Text(
+                      'Read Free Books',
+                      textAlign: TextAlign.center,
+                      style: TextStyle(
+                        fontSize: 36,
+                        fontFamily: 'Pacifico',
+                      ),
+                    ),
+                  );
+                });
+          })
         ],
       ),
     );
