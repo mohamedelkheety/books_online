efd0631505537e97a82abe14e4e12f5157203dfe
mohamedelkheety <mohamedelkheaty97@gmail.com>
2024-01-04 10:58:56 +0200

    create book entity

---
+++ lib/Features/home/domain/entities/book_entity.dart
@@ -0,0 +1,11 @@
+class BookEntity {
+  final String title, auther, image;
+  final num rating, price;
+
+  BookEntity(
+      {required this.title,
+      required this.auther,
+      required this.image,
+      required this.rating,
+      required this.price});
+}
