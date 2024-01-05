64cd3e316c2461347f52d43da172f3ba0c94802b
mohamedelkheety <mohamedelkheaty97@gmail.com>
2024-01-05 23:42:36 +0200

    create factory constractor in book Entity

--- lib/Features/home/domain/entities/book_entity.dart
+++ lib/Features/home/domain/entities/book_entity.dart
@@ -1,11 +1,20 @@
 class BookEntity {
   final String title, auther, image;
-  final num rating, price;
+  final num rating;
 
-  BookEntity(
-      {required this.title,
-      required this.auther,
-      required this.image,
-      required this.rating,
-      required this.price});
+  BookEntity({
+    required this.title,
+    required this.auther,
+    required this.image,
+    required this.rating,
+  });
+
+  factory BookEntity.fromJson(json) {
+    return BookEntity(
+      title: json["items"]["volumeInfo"]["title"],
+      auther: json["items"]["volumeInfo"]["authors"][0],
+      image: json["items"]["volumeInfo"]["imageLinks"]["thumbnail"],
+      rating: json["items"]["volumeInfo"]["ratingsCount"],
+    );
+  }
 }
